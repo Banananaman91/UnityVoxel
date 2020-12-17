e1d8050a26da1db7f81eae585e5747eb265a7bb1
Ian <barrowian1991@gmail.com>
2020-12-17 17:06:43 +0000

    Updated controller and scene for rudimentary 3rd person controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,11 +12,9 @@ public class Controller : MonoBehaviour
     {
         _moveDirection.x = Input.GetAxis("Horizontal");
         _moveDirection.z = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.Space)) _moveDirection.y = 1;
-        else if (Input.GetKey((KeyCode.LeftShift))) _moveDirection.y = -1;
-        else _moveDirection.y = 0;
         float moveRotation = Input.GetAxis("Mouse X") * _rotateSpeed;
         transform.Rotate(0, moveRotation, 0);
-        transform.position += _moveDirection * (_movementSpeed * Time.deltaTime);
+        var directionVector = (transform.right * _moveDirection.x) + (transform.forward * _moveDirection.z);
+        transform.position += directionVector * (_movementSpeed * Time.deltaTime);
     }
 }
